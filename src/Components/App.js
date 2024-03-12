a6b377ab49e93e2acdbceb151708bb472efdbf56
AnnPrim <Primpraowann@gmail.com>
2024-03-12 12:01:00 -0500

    page style and responsive, still working on randomize data on the screen

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,36 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ResultSection } from './Results/ResultSection';
 import { SummarySection } from './Summary/Summary';
+import { FooterSection } from './Footer/Footer';
 import data from '../data.json'
-
 
 import '../Styles/Main.css';
 
 function App() {
-  const [idx, setIndx] = useState(0)
-  const [newData, setNewData] = useState(data[idx]);
-
+  const [idx, setIdx] = useState(0);
+  const [newData, setNewData] = useState(data[0]);
 
   const rotateData = () => {
     console.log(idx)
     console.log(data.length)
-    if (idx > data.length) {
-        setIndx((prev) => prev - 1);
+    if (idx === data.length - 1) {
         setNewData(data[idx]);
+        return setIdx(0);
     }
-      setIndx((prev) => prev + 1);
-      setNewData(data[idx]);
+    console.log('out if', idx)
+        setNewData(data[idx]);
+        setIdx((prev) => prev + 1);
 
   };
 
   return (
-    <div className="App">
+      <>
+        <div className="App">
 
-       <ResultSection newData={newData}/>
+          <ResultSection newData={newData}/>
 
-       <SummarySection rotateData={rotateData}/>
+          <SummarySection rotateData={rotateData}/>
 
-    </div>
+        </div>
+
+        <FooterSection />
+      </>
+
+
   );
 }
 
